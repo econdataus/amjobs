3861c9e239c222ec4d4f477632f1c9ecf8fbfb19
econdataus <admin@econdataus>
2015-05-22 21:50:25 -0700

    Output truncated numbers for 183 and 262 and first line of erroneous numbers for 183.

--- amstem183 - Copy.R
+++ amstem183 - Copy.R
@@ -38,99 +38,13 @@ getslopes <- function(n, jj, year0, year1, year2, year3) {
   line <- sprintf("%s  %4d", line, year1)
 }
 
-getjob <- function(jj, year1, year2) {
-  #kk <- jj[jj$emp_imm > 0,]
-  kk <- jj[!is.na(jj$emp_imm),]
-  kk <- kk[kk$year >= year1 & kk$year <= year2,]
-  #kk <- jj[jj$year >= year1 & jj$year <= year2,]
-  mm <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc, weights=weight)))
-  pp <- sum(kk$emp_native) / sum(kk$emp_imm) * mm$coef[2] * 100 #actual slope
-  #pp <- (sum(kk$emp_native) / sum(kk$emp_imm)) * (floor(1000 * mm$coef[2]) / 10) # trucated slope
-  ret <- sprintf("%7.1f", pp)
-  #print(paste(ret, year1, year2))
-  return(ret)
-}
-
-getjobs <- function(n, jj, year0, year1, year2, year3) {
-  line <- sprintf("%4d", year1)
-  if (year1 >= year0)
-  {
-    for (yr in year0:year1)
-    {
-      line <- paste(line, "       ")
-    }
-  }
-  for (yr in year2:year3)
-  {
-    line <- paste(line, getjob(jj, year1, yr))
-  }
-  line <- sprintf("%s  %4d", line, year1)
-}
-
-getjob_trunc <- function(jj, year1, year2) {
-  #kk <- jj[jj$emp_imm > 0,]
-  kk <- jj[!is.na(jj$emp_imm),]
-  kk <- kk[kk$year >= year1 & kk$year <= year2,]
-  #kk <- jj[jj$year >= year1 & jj$year <= year2,]
-  mm <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc, weights=weight)))
-  #pp <- sum(kk$emp_native) / sum(kk$emp_imm) * mm$coef[2] * 100 # actual slope
-  pp <- (sum(kk$emp_native) / sum(kk$emp_imm)) * (floor(1000 * mm$coef[2]) / 10) # trucated slope
-  ret <- sprintf("%7.1f", pp)
-  #print(paste(ret, year1, year2))
-  return(ret)
-}
-
-getjobs_trunc <- function(n, jj, year0, year1, year2, year3) {
-  line <- sprintf("%4d", year1)
-  if (year1 >= year0)
-  {
-    for (yr in year0:year1)
-    {
-      line <- paste(line, "       ")
-    }
-  }
-  for (yr in year2:year3)
-  {
-    line <- paste(line, getjob_trunc(jj, year1, yr))
-  }
-  line <- sprintf("%s  %4d", line, year1)
-}
-
-getjob_err <- function(jj, year1, year2) {
-  #kk <- jj[jj$emp_imm > 0,]
-  ee <- jj[jj$year >= (year1-1) & jj$year <= year2,]
-  kk <- jj[!is.na(jj$emp_imm),]
-  kk <- kk[kk$year >= year1 & kk$year <= year2,]
-  mm <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc, weights=weight)))
-  #pp <- sum(kk$emp_native) / sum(kk$emp_imm) * mm$coef[2] * 100 #actual slope
-  #pp <- (sum(kk$emp_native) / sum(kk$emp_imm)) * (floor(1000 * mm$coef[2]) / 10) # trucated slope
-  pp <- (sum(ee$emp_native) / sum(kk$emp_imm)) * (floor(1000 * mm$coef[2]) / 10) # trucated slope with error
-  ret <- sprintf("%7.1f", pp)
-  #print(paste(ret, year1, year2))
-  return(ret)
-}
-
-getjobs_err <- function(n, jj, year0, year1, year2, year3) {
-  line <- sprintf("%4d", year1)
-  if (year1 >= year0)
-  {
-    for (yr in year0:year1)
-    {
-      line <- paste(line, "       ")
-    }
-  }
-  for (yr in year2:year3)
-  {
-    line <- paste(line, getjob_err(jj, year1, yr))
-  }
-  line <- sprintf("%s  %4d", line, year1)
-}
-
 library(foreign)
 library(plyr)
 
 dd <- read.dta("public_h1b.dta")
 dd <- dd[dd$year != 2007,] # DROP 2007 DUE TO ERRORS
+d0 <- dd
+dd <- dd[dd$year >= 2001,] # NO H1B DATA FOR 2000
 source("amjobsg.R")
 
 dd$emp_imm <- dd$h1b
@@ -144,8 +58,6 @@ dd$weight = dd$pop_native / dd$pop_native_t
 dd$fyear <- as.factor(dd$year)
 dd$floc <- as.factor(dd$fips)
 dd$emp_imm_1000 <- dd$emp_imm / 1000
-d0 <- dd
-dd <- dd[dd$year >= 2001,] # NO H1B DATA FOR 2000
 
 labyears="2001-2010" # CHANGE FOR YEARS
 labyears2="2002-2010"
@@ -153,10 +65,8 @@ labyears2="2002-2010"
 print("H1B AND NATIVES: NUMBER EMPLOYED AND LEVELS, BY YEAR")
 print("")
 ss <- ddply(dd, "year", summarise, emp_imm=sum(emp_imm), emp_native=sum(emp_native), emp_total=sum(emp_total), pop_native=sum(pop_native))
-ss$imm_level <- 100* ss$emp_imm / ss$emp_total
-ss$nat_level <- 100* ss$emp_native / ss$pop_native
-ss$ln_imm_level <- log(ss$imm_level)
-ss$ln_nat_level <- log(ss$nat_level)
+ss$imm_level <- ss$emp_imm / ss$emp_total
+ss$nat_level <- ss$emp_native / ss$pop_native
 print(ss)
 
 print("")
@@ -257,87 +167,34 @@ print(sprintf("%2d) %9.4f %9.4f %8.1f %8.4f  %s",
   nn, mm$coef[1], mm$coef[2], jj, with(kk, cor(ln_imm_level, ln_nat_level)), ll))
 lst[nn] <- reginfo(nn, kk, ll, mm)
 
-ff <- dd
-if (max(ff$year) >= 2010)
+kk <- dd
+if (max(kk$year) >= 2010)
 {
 print("")
-print("SLOPE BETWEEN GIVEN YEARS (using same regression as was used to obtain 183 job finding)")
+print("SLOPE BETWEEN GIVEN YEARS (using same regression as was used to obtain 183 job finding)  ")
 print("----  ------  ------  ------  ------  ------  ------  ------  ------  ----")
 print("year    2003    2004    2005    2006    2007    2008    2009    2010  year")
 print("----  ------  ------  ------  ------  ------  ------  ------  ------  ----")
-print( getslopes(1, ff, 2002, 2001, 2003, 2010))
-print( getslopes(1, ff, 2002, 2002, 2004, 2010))
-print( getslopes(1, ff, 2002, 2003, 2005, 2010))
-print( getslopes(1, ff, 2002, 2004, 2006, 2010))
-print( getslopes(1, ff, 2002, 2005, 2007, 2010))
-print( getslopes(1, ff, 2002, 2006, 2008, 2010))
-print( getslopes(1, ff, 2002, 2007, 2009, 2010))
-print( getslopes(1, ff, 2002, 2008, 2010, 2010))
-print("")
-print("NATIVE JOBS GAINED/LOST BETWEEN GIVEN YEARS (with incorrect native employment in 1st row and truncation errors in all rows)")
-print("----  ------  ------  ------  ------  ------  ------  ------  ------  ----")
-print("year    2003    2004    2005    2006    2007    2008    2009    2010  year")
-print("----  ------  ------  ------  ------  ------  ------  ------  ------  ----")
-print( getjobs_err(1, d0, 2002, 2001, 2003, 2010))
-print( getjobs_trunc(1, ff, 2002, 2001, 2003, 2010))
-print( getjobs_trunc(1, ff, 2002, 2002, 2004, 2010))
-print( getjobs_trunc(1, ff, 2002, 2003, 2005, 2010))
-print( getjobs_trunc(1, ff, 2002, 2004, 2006, 2010))
-print( getjobs_trunc(1, ff, 2002, 2005, 2007, 2010))
-print( getjobs_trunc(1, ff, 2002, 2006, 2008, 2010))
-print( getjobs_trunc(1, ff, 2002, 2007, 2009, 2010))
-print( getjobs_trunc(1, ff, 2002, 2008, 2010, 2010))
-print("")
-print("NATIVE JOBS GAINED/LOST PER EACH 100 NEW H-1B WORKERS BETWEEN GIVEN YEARS (using the study's methodology with no errors)")
-print("----  ------  ------  ------  ------  ------  ------  ------  ------  ----")
-print("year    2003    2004    2005    2006    2007    2008    2009    2010  year")
-print("----  ------  ------  ------  ------  ------  ------  ------  ------  ----")
-print( getjobs(1, ff, 2002, 2001, 2003, 2010))
-print( getjobs(1, ff, 2002, 2002, 2004, 2010))
-print( getjobs(1, ff, 2002, 2003, 2005, 2010))
-print( getjobs(1, ff, 2002, 2004, 2006, 2010))
-print( getjobs(1, ff, 2002, 2005, 2007, 2010))
-print( getjobs(1, ff, 2002, 2006, 2008, 2010))
-print( getjobs(1, ff, 2002, 2007, 2009, 2010))
-print( getjobs(1, ff, 2002, 2008, 2010, 2010))
-}
-
-state <- unique(dd$state)
-tt = data.frame(state)
-
-year1 <- 2003
-year2 <- 2008
-yy <- dd[dd$year >= year1 & dd$year <= year2,]
-nn <- 0
-print("")
-
-for (i in 1:nrow(tt))
-{
-  st <- tt$state[i]
-  kk <- yy[yy$state == st,]
-  nn <- i
-  ll <- paste(year1,"-",year2,", longest span of growing h1b level", sep="")
-  #mm <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc, weights=weight)))
-  #mm <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear, weights=weight)))
-  mm <- (with(kk, lm(ln_nat_level ~ ln_imm_level)))
-  jj <- sum(kk$emp_native) / sum(kk$emp_imm) * mm$coef[2] * 100
-  print(sprintf("%2d) %9.4f %9.4f %8.1f %8.4f  %s",
-                nn, mm$coef[1], mm$coef[2], jj, with(kk, cor(ln_imm_level, ln_nat_level)), st))
-  #lst[nn] <- reginfo(nn, kk, ll, mm)
+print( getslopes(1, kk, 2002, 2001, 2003, 2010))
+print( getslopes(1, kk, 2002, 2002, 2004, 2010))
+print( getslopes(1, kk, 2002, 2003, 2005, 2010))
+print( getslopes(1, kk, 2002, 2004, 2006, 2010))
+print( getslopes(1, kk, 2002, 2005, 2007, 2010))
+print( getslopes(1, kk, 2002, 2006, 2008, 2010))
+print( getslopes(1, kk, 2002, 2007, 2009, 2010))
+print( getslopes(1, kk, 2002, 2008, 2010, 2010))
 }
 
 #forlab1 <- "Employed Foreign STEM Workers with Adv US Degrees"
 #forlab2 <- "Foreign Share of Total Employment (STEM w/ Adv US Deg)"
 forlab1 <- "Thousands of H-1B Certified Workers"
-forlab2 <- "H-1B Certification Level"
+forlab2 <- "Foreign Share of Total Employment (STEM w/ Adv US Deg)"
 
-x11()
-# Graph Native versus H-1B Certified Workers
-kk <- dd
+# Graph Employed Natives vs. Employed Immigrants (foreign STEM US adv. degrees)
 nn <- 1
 with(kk, plot(emp_native/1000000 ~ emp_imm_1000, cex=0.5, pch=kk$sPch, col=kk$sCol,
   ylab="Millions of Employed Native Workers", xlab=forlab1))
-title(main=paste("Figure ", nn, ": Native versus H-1B Certified Workers, ", labyears, sep=""))
+title(main=paste("Figure ", nn, ": Native versus H-1B Certified Workers, ", labyears1, sep=""))
 with(kk, text(emp_imm_1000, emp_native/1000000, labels=year-2000, cex=0.5, pos=4))
 legend("bottomright", inset=0, title="State",
 c("California","Connecticut","DC","Florida","Georgia","Illinois","Maryland","Massachusetts","Michigan","New Jersey","New York","Ohio","Oregon","Pennsylvania","Texas","Virginia","Washington"),
@@ -345,14 +202,13 @@ cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE)
 grid()
 readline("Press enter to continue, escape to exit")
 
-labyears <- "2002-2010"
 x11()
-# Graph Native versus H-1B Certified Workers
+# Graph Employed Natives vs. Employed Immigrants (foreign STEM US adv. degrees)
 kk <- dd[dd$year >= 2002,]
 nn <- nn+1
 with(kk, plot(emp_native/1000000 ~ emp_imm_1000, cex=0.5, pch=kk$sPch, col=kk$sCol,
   ylab="Millions of Employed Native Workers", xlab=forlab1))
-title(main=paste("Figure ", nn, ": Native versus H-1B Certified Workers, ", labyears, sep=""))
+title(main=paste("Figure ", nn, ": Native versus H-1B Certified Workers, ", labyears2, sep=""))
 with(kk, text(emp_imm_1000, emp_native/1000000, labels=year-2000, cex=0.5, pos=4))
 legend("bottomright", inset=0, title="State",
 c("California","Connecticut","DC","Florida","Georgia","Illinois","Maryland","Massachusetts","Michigan","New Jersey","New York","Ohio","Oregon","Pennsylvania","Texas","Virginia","Washington"),
@@ -360,14 +216,15 @@ cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE)
 grid()
 readline("Press enter to continue, escape to exit")
 
-kk <- dd
-labyears <- "2001-2010"
+forlab2 <- "H-1B Certification Level"
+kk <- kk1
+labyears <- labyears1
 x11()
-# Graph Native Employment Level vs. H-1B Certification Level
+# Graph Native Employment Rate vs. H-1B Certification Level
 nn <- nn+1
-with(kk, plot(nat_level ~ imm_level, cex=0.5, pch=kk$sPch, col=kk$sCol,
-  ylab="Native Worker Employment Level", xlab=forlab2, xlim=c(0,0.04)))
-title(main=paste("Figure ", nn, ": Native Employment Level vs. H-1B Certification Level\n", labyears, sep=""))
+with(kk, plot(emp_pop ~ h1b_level_emp, cex=0.5, pch=kk$sPch, col=kk$sCol,
+  ylab="Native Worker Employment Rate", xlab=forlab2, xlim=c(0,0.04)))
+title(main=paste("Figure ", nn, ": Native Employment Rate vs. H-1B Certification Level\n", labyears, sep=""))
 #with(kk, text(h1b_level_emp, emp_pop, labels=kk$ss, cex=0.5, pos=4))
 legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
   c("California","Connecticut","DC","Florida","Georgia","Illinois","Maryland","Massachusetts","Michigan","New Jersey","New York","Ohio","Oregon","Pennsylvania","Texas","Virginia","Washington"))
@@ -379,7 +236,7 @@ readline("Press enter to continue, escape to exit")
 x11()
 # Graph Natural Logs of Native Employment Rate vs. H-1B Certification Level
 nn <- nn+1
-with(kk, plot(ln_nat_level ~ ln_imm_level, cex=0.5, pch=kk$sPch, col=kk$sCol,
+with(kk, plot(emp_ln_pop ~ h1b_ln_emp, cex=0.5, pch=kk$sPch, col=kk$sCol,
   ylab="Log of Native Worker Employment Rate", xlab=paste("Log of", forlab2)))
 title(main=paste("Figure ", nn, ": Native Employment Rate vs. H-1B Certification Level\n", labyears, sep=""))
 legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
@@ -395,7 +252,7 @@ readline("Press enter to continue, escape to exit")
 x11()
 # Graph Natural Logs of Native Employment Rate vs. H-1B Certification Level
 nn <- nn+1
-with(kk, plot(ln_nat_level ~ ln_imm_level, cex=0.5, pch=kk$sPch, col=kk$sCol,
+with(kk, plot(emp_ln_pop ~ h1b_ln_emp, cex=0.5, pch=kk$sPch, col=kk$sCol,
   ylab="Log of Native Worker Employment Rate", xlab=paste("Log of", forlab2)))
 title(main=paste("Figure ", nn, ": Native Employment Rate vs. H-1B Certification Level\n", labyears, sep=""))
 legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
@@ -403,18 +260,18 @@ legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
 grid()
 legend("bottomleft", inset=0, cex=0.5, lty=c(2,1), col=c("red","black"), horiz=FALSE,
   c("w/o weighting","with weighting"))
-lma <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc)))
+lma <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips)))
 abline(lma, col="red", lty=2)
-lmb <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc, weights=weight)))
+lmb <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips, weights=weight)))
 abline(lmb)
 readline("Press enter to continue, escape to exit")
 
-kk <- dd[dd$year >= 2003 & dd$year <= 2006,]
+kk <- d1[d1$year >= 2003 & d1$year <= 2006,]
 labyears <- "2003-2006"
 x11()
 # Graph Natural Logs of Native Employment Rate vs. H-1B Certification Level
 nn <- nn+1
-with(kk, plot(ln_nat_level ~ ln_imm_level, cex=0.5, pch=kk$sPch, col=kk$sCol,
+with(kk, plot(emp_ln_pop ~ h1b_ln_emp, cex=0.5, pch=kk$sPch, col=kk$sCol,
   ylab="Log of Native Worker Employment Rate", xlab=paste("Log of", forlab2)))
 title(main=paste("Figure ", nn, ": Native Employment Rate vs. H-1B Certification Level\n", labyears, sep=""))
 legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
@@ -422,16 +279,16 @@ legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
 grid()
 #legend("bottomright", inset=0, cex=0.5, lty=c(2,1), col=c("red","black"), horiz=FALSE,
 #  c("w/o weighting","with weighting"))
-#lma <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc, weights=weight)))
+#lma <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips, weights=weight)))
 #abline(lma)
 readline("Press enter to continue, escape to exit")
 
-kk <- dd[dd$year >= 2003 & dd$year <= 2006,]
+kk <- d1[d1$year >= 2003 & d1$year <= 2006,]
 labyears <- "2003-2006"
 x11()
 # Graph Natural Logs of Native Employment Rate vs. H-1B Certification Level
 nn <- nn+1
-with(kk, plot(ln_nat_level ~ ln_imm_level, cex=0.5, pch=kk$sPch, col=kk$sCol,
+with(kk, plot(emp_ln_pop ~ h1b_ln_emp, cex=0.5, pch=kk$sPch, col=kk$sCol,
   ylab="Log of Native Worker Employment Rate", xlab=paste("Log of", forlab2)))
 title(main=paste("Figure ", nn, ": Native Employment Rate vs. H-1B Certification Level\n", labyears, sep=""))
 legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
@@ -439,7 +296,46 @@ legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
 grid()
 legend("bottomleft", inset=0, cex=0.5, lty=c(2,1), col=c("red","black"), horiz=FALSE,
   c("w/o weighting","with weighting"))
-lma <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc)))
+lma <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips)))
 abline(lma, col="red", lty=2)
-lmb <- (with(kk, lm(ln_nat_level ~ ln_imm_level + fyear + floc, weights=weight)))
+lmb <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips, weights=weight)))
 abline(lmb)
+readline("Press enter to continue, escape to exit")
+
+kk <- d1[d1$year >= 2002 & d1$year <= 2008,]
+labyears <- "2002-2008"
+x11()
+# Graph Natural Logs of Native Employment Rate vs. H-1B Certification Level
+nn <- nn+1
+with(kk, plot(emp_ln_pop ~ h1b_ln_emp, cex=0.5, pch=kk$sPch, col=kk$sCol,
+  ylab="Log of Native Worker Employment Rate", xlab=paste("Log of", forlab2)))
+title(main=paste("Figure ", nn, ": Native Employment Rate vs. H-1B Certification Level\n", labyears, sep=""))
+legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
+  c("California","Connecticut","DC","Florida","Georgia","Illinois","Maryland","Massachusetts","Michigan","New Jersey","New York","Ohio","Oregon","Pennsylvania","Texas","Virginia","Washington"))
+grid()
+legend("bottomleft", inset=0, cex=0.5, lty=c(2,1), col=c("red","black"), horiz=FALSE,
+  c("w/o weighting","with weighting"))
+lma <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips)))
+abline(lma, col="red", lty=2)
+lmb <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips, weights=weight)))
+abline(lmb)
+readline("Press enter to continue, escape to exit")
+
+kk <- d1[d1$year >= 2003 & d1$year <= 2005,]
+labyears <- "2003-2005"
+x11()
+# Graph Natural Logs of Native Employment Rate vs. H-1B Certification Level
+nn <- nn+1
+with(kk, plot(emp_ln_pop ~ h1b_ln_emp, cex=0.5, pch=kk$sPch, col=kk$sCol,
+  ylab="Log of Native Worker Employment Rate", xlab=paste("Log of", forlab2)))
+title(main=paste("Figure ", nn, ": Native Employment Rate vs. H-1B Certification Level\n", labyears, sep=""))
+legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
+  c("California","Connecticut","DC","Florida","Georgia","Illinois","Maryland","Massachusetts","Michigan","New Jersey","New York","Ohio","Oregon","Pennsylvania","Texas","Virginia","Washington"))
+grid()
+legend("bottomleft", inset=0, cex=0.5, lty=c(2,1), col=c("red","black"), horiz=FALSE,
+  c("w/o weighting","with weighting"))
+lma <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips)))
+abline(lma, col="red", lty=2)
+lmb <- (with(kk, lm(emp_ln_pop ~ h1b_ln_emp + fyear + ffips, weights=weight)))
+abline(lmb)
+readline("Press enter to continue, escape to exit")
--- amstem262.R
+++ amstem262.R
@@ -67,6 +67,36 @@ getjobs <- function(n, jj, year0, year1, year2, year3) {
   line <- sprintf("%s  %4d", line, year1)
 }
 
+getjob_trunc <- function(jj, year1, year2) {
+  ii <- jj[jj$year >= year1 & jj$year <= year2,]
+  kk <- ii[ii$emp_imm > 0,]
+  kk <- kk[kk$emp_imm2 > 0,]
+  #kk <- kk[!is.na(kk$emp_imm),]
+  mm <- (with(kk, lm(ln_nat_level ~ ln_imm_level + ln_imm_level2 + fyear + floc, weights=weight)))
+  #pp <- sum(ii$emp_native) / sum(ii$emp_imm) * mm$coef[2] * 100
+  pp <- (sum(ii$emp_native) / sum(ii$emp_imm)) * (floor(1000 * mm$coef[2]) / 10) # trucated slope
+  ret <- sprintf("%7.1f", pp)
+  #ret <- sprintf("%7.2f", pp) # 2 decimal places so that 262 shown
+  #print(paste(ret, year1, year2))
+  return(ret)
+}
+
+getjobs_trunc <- function(n, jj, year0, year1, year2, year3) {
+  line <- sprintf("%4d", year1)
+  if (year1 >= year0)
+  {
+    for (yr in year0:year1)
+    {
+      line <- paste(line, "       ")
+    }
+  }
+  for (yr in year2:year3)
+  {
+    line <- paste(line, getjob_trunc(jj, year1, yr))
+  }
+  line <- sprintf("%s  %4d", line, year1)
+}
+
 library(foreign)
 library(plyr)
 
@@ -222,7 +252,23 @@ print( getslopes(1, kk, 2001, 2008, 2011, 2013))
 print( getslopes(1, kk, 2001, 2009, 2012, 2013))
 print( getslopes(1, kk, 2001, 2010, 2013, 2013))
 print("")
-print("JOBS GAINED/LOST BETWEEN GIVEN YEARS (using same regression as was used to obtain 262 job finding)")
+print("NATIVE JOBS GAINED/LOST BETWEEN GIVEN YEARS (using same regression as was used to obtain 262 job finding but with truncation errors)")
+print("----  ------  ------  ------  ------  ------  ------  ------  ------  ------  ------  ------  ----")
+print("year    2003    2004    2005    2006    2007    2008    2009    2010    2011    2012    2013  year")
+print("----  ------  ------  ------  ------  ------  ------  ------  ------  ------  ------  ------  ----")
+print( getjobs_trunc(1, kk, 2001, 2000, 2003, 2013))
+print( getjobs_trunc(1, kk, 2001, 2001, 2004, 2013))
+print( getjobs_trunc(1, kk, 2001, 2002, 2005, 2013))
+print( getjobs_trunc(1, kk, 2001, 2003, 2006, 2013))
+print( getjobs_trunc(1, kk, 2001, 2004, 2007, 2013))
+print( getjobs_trunc(1, kk, 2001, 2005, 2008, 2013))
+print( getjobs_trunc(1, kk, 2001, 2006, 2009, 2013))
+print( getjobs_trunc(1, kk, 2001, 2007, 2010, 2013))
+print( getjobs_trunc(1, kk, 2001, 2008, 2011, 2013))
+print( getjobs_trunc(1, kk, 2001, 2009, 2012, 2013))
+print( getjobs_trunc(1, kk, 2001, 2010, 2013, 2013))
+print("")
+print("NATIVE JOBS GAINED/LOST PER EACH 100 STEM WORKERS WITH ADVANCED US DEGREES BETWEEN GIVEN YEARS (using study's methodology with no errors)")
 print("----  ------  ------  ------  ------  ------  ------  ------  ------  ------  ------  ------  ----")
 print("year    2003    2004    2005    2006    2007    2008    2009    2010    2011    2012    2013  year")
 print("----  ------  ------  ------  ------  ------  ------  ------  ------  ------  ------  ------  ----")
@@ -241,3 +287,21 @@ print( getjobs(1, kk, 2001, 2010, 2013, 2013))
 
 forlab1 <- "Employed Foreign STEM Workers with Adv US Degrees"
 forlab2 <- "Foreign Share of Total Employment (STEM w/ Adv US Deg)"
+labyears="2000-2013" # CHANGE FOR YEARS
+
+x11()
+# Graph Natural Logs of Native Employment Rate vs. Immigrant Share
+kk <- dd
+nn <- 1
+with(kk, plot(ln_nat_level ~ ln_imm_level, cex=0.5, pch=kk$sPch, col=kk$sCol,
+  ylab="Log of Native Worker Employment Rate", xlab=paste("Log of", forlab2)))
+title(main=paste("Figure ", nn, ": Native Employment Rate vs. Immigrant Share\n", labyears, sep=""))
+#legend("bottomright", inset=0, cex=0.5, pch=gg$pch, col=gg$col, horiz=FALSE,
+#  c("California","Connecticut","DC","Florida","Georgia","Illinois","Maryland","Massachusetts","Michigan","New Jersey","New York","Ohio","Oregon","Pennsylvania","Texas","Virginia","Washington"))
+grid()
+#legend("bottomright", inset=0, cex=0.5, lty=c(2,1), col=c("red","black"), horiz=FALSE,
+#  c("w/o weighting","with weighting"))
+#lma <- (with(kk, lm(lnemprate_native ~ lnemp_immshare)))
+#abline(mm3, col="red", lty=2)
+#abline(mm4)
+readline("Press enter to continue, escape to exit")
